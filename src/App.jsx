f26a0a0d34d97e9e990721dedf0f954d7207345e
Mathew Grabau <mgrabau@mathewgrabau.ca>
2023-08-21 19:17:34 -0500

    In progress building up the app (need to fix tooling)

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom";
+/* global document */
+// Added just to get the eslint highlighting to process correctly.
+import { createRoot } from "react-dom";
 
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
+import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      animal: "Dog",
-      name: "Luna",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      animal: "Bird",
-      name: "Pepper",
-      breed: "Parrot",
-    }),
-    React.createElement(Pet, {
-      animal: "Cat",
-      name: "Meow",
-      breed: "Mixed",
-    }),
-  ]);
+  return (<div>
+    <h1>Adopt Me!</h1>
+    <Pet name="Luna" animal="Dog" breed="Havanese" />
+    <Pet name="Pepper" animal="Bird" breed="Parrot" />
+    <Pet name="Meow" animal="Cat" breed="Mixed" />
+  </div>);
 };
 
 // Get the container
 const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
+const root = createRoot(container);
 // This actually renders the App element into it.
-root.render(React.createElement(App));
+root.render(<App />);
