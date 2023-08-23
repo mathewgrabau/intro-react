f5232e225a486efb07fae9de3c76601954dd2704
Mathew Grabau <mgrabau@mathewgrabau.ca>
2023-08-22 19:07:04 -0500

    Added effect

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import Pet from "./Pet";
+import useBreedList from "./useBreedList";
 /* global fetch */
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
@@ -12,7 +13,8 @@ const SearchParams = () => {
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
   const [pets, setPets] = useState([]);
-  const breeds = [];
+  // Some people make custom hooks for everything; interesting concept
+  const [breeds] = useBreedList(animal);
 
   // Without dependencies, it will do it every single time the component is rendered.
   // With the empty array it means run it once and run it only on the initial rendering.
@@ -28,6 +30,9 @@ const SearchParams = () => {
 
     setPets(json.pets);
   }
+
+  // Note that this is a controlled form, and is not considered best practice.
+  // Instead, can use the uncontrolled form and grab the form values to process it
 
   return (
     <div className="search-params">
