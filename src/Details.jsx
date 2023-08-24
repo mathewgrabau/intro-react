63532ef6f2dc25f90bbe6e0476941ef55c1b6e05
Mathew Grabau <mgrabau@mathewgrabau.ca>
2023-08-24 04:23:50 -0500

    Refactored fetchBreedList to clean it up

--- src/Details.jsx
+++ src/Details.jsx
@@ -6,27 +6,31 @@ const Details = () => {
   const { id } = useParams();
   // The key is arbitrary.
   // Means run the fetchPet if id is not in the cache
-    const results = useQuery(["pet", id], fetchPet);
+  const results = useQuery(["pet", id], fetchPet);
 
-    // isLoading = the first one
-    if (results.isLoading) {
-        return (
-            <div className="loading-pane">
-                <h2 className="loader">--</h2>
-            </div>
-        )
-    }
+  // isLoading = the first one
+  if (results.isLoading) {
+    return (
+      <div className="loading-pane">
+        <h2 className="loader">--</h2>
+      </div>
+    );
+  }
 
-    const pet = results.data.pets[0];
+  const pet = results.data.pets[0];
 
-    return (<div className="details">
-        <div>
-            <h1>{pet.name}</h1>
-            <h2>{pet.animal} - {pet.breed} - {pet.city}, {pet.state}</h2>
-            <button>Adopt {pet.name}</button>
-            <p>{pet.description}</p>
-        </div>
-    </div>);
+  return (
+    <div className="details">
+      <div>
+        <h1>{pet.name}</h1>
+        <h2>
+          {pet.animal} - {pet.breed} - {pet.city}, {pet.state}
+        </h2>
+        <button>Adopt {pet.name}</button>
+        <p>{pet.description}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Details;
---
+++ src/fetchBreedList.js
@@ -0,0 +1,25 @@
+// Contains the query definition for it
+const fetchBreedList = async ({ queryKey }) => {
+  const animal = queryKey[1];
+
+  if (!animal) {
+    return [];
+  }
+
+  // eslint-disable-next-line no-undef
+  const apiResult = await fetch(
+    `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+  );
+
+  if (!apiResult.ok) {
+    // React query will catch this error and set the error state. It is the way to pass that along/trigger it.
+    // Note that it is for debugging, so can put the inforamtion in the error message.
+    throw new Error(`breeds/${animal} fetch failed - did not return ok`);
+  }
+
+  // Returning the json from the apiResult, don't need to await because it is returning a Promise.
+  return apiResult.json();
+};
+
+export default fetchBreedList;
+// Note that this is useful outside of React query as well (it is testable without the dependency on React query)
