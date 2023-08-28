dd535e4a4389c7f627dbcc92534c8e98783eb1a3
Mathew Grabau <mgrabau@mathewgrabau.ca>
2023-08-28 01:16:39 -0500

    Rendering the image based on the context

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { useQuery } from "@tanstack/react-query";
 import useBreedList from "./useBreedList";
 import fetchSearch from "./fetchSearch";
 import Results from "./Results";
+import AdoptedPetContext from "./AdoptedPetContext";
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
@@ -22,6 +23,7 @@ const SearchParams = () => {
   const [animal, setAnimal] = useState("");
   // Some people make custom hooks for everything; interesting concept
   const [breeds] = useBreedList(animal);
+  const [adoptedPet] = useContext(AdoptedPetContext); // Just ignoring the setter part of the hook (destructing the first element)
 
   const results = useQuery(["search", requestParams], fetchSearch);
   const pets = results?.data?.pets ?? [];
@@ -44,6 +46,13 @@ const SearchParams = () => {
           setRequestParams(obj);
         }}
       >
+        {
+          adoptedPet? (
+            <div className="pet iamge-container">
+              <img src={adoptedPet.images[0]} alt={adoptedPet.name} />
+            </div>
+          ) : null
+        }
         <label htmlFor="location">
           Location
           <input
