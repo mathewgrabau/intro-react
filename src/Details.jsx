374f803ae8a9827986cb4e53236037af5c90ac51
Mathew Grabau <mgrabau@mathewgrabau.ca>
2023-08-28 01:12:12 -0500

    Context added.

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
-import { useParams } from "react-router-dom";
+import { useState, useContext } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import fetchPet from "./fetchPet";
 
+import AdoptedPetContext from "./AdoptedPetContext";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
 import Modal from "./Modal";
@@ -10,6 +11,12 @@ import Modal from "./Modal";
 const Details = () => {
   // For showing the modal
   const [showModal, setShowModal] = useState(false);
+  // For programmatic navigation
+  const navigate = useNavigate();
+  // For the context, and we are ignoring the read part.
+  // eslint-disable-next-line no-unused-vars
+  const [_, setAdoptedPet] = useContext(AdoptedPetContext);
+
   const { id } = useParams();
   // The key is arbitrary.
   // Means run the fetchPet if id is not in the cache
@@ -42,7 +49,10 @@ const Details = () => {
                 <div>
                     <h1>Would you like to adopt {pet.name}?</h1>
                     <div className="buttons">
-                        <button>Yes</button>
+                        <button onClick={() => {
+                            setAdoptedPet(pet);
+                            navigate("/");
+                        }}>Yes</button>
                         <button onClick={() => setShowModal(false)}>No</button>
                     </div>
                 </div>
