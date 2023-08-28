051da05bf3a8eaaabb868baf5b642ae8ecb20c46
Mathew Grabau <mgrabau@mathewgrabau.ca>
2023-08-28 01:12:08 -0500

    Started wiring in the context. Got it setting.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 /* global document */
 // Added just to get the eslint highlighting to process correctly.
+import { useState } from "react";
 import { createRoot } from "react-dom/client";
 import { Link, Routes, Route, BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+import AdoptedPetContext from "./AdoptedPetContext";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
 
@@ -21,18 +23,23 @@ const queryClient = new QueryClient({
 // While it looks awkward, it does allow control around the context exposed to a given area.
 
 const App = () => {
+  const adoptedPetHook = useState(null);
+  // The provider is like a wormhole - you can put whatever you want in it, and it's available. It's a way of getting the information to move around.
+  // The whole hook is being passed, so that the context can be updated. It is read and write. The provider doesn't care.
   return (
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <div>
-          <header>
-            <Link to="/">Adopt Me!</Link>
-          </header>
-          <Routes>
-            <Route path="/details/:id" element={<Details />} />
-            <Route path="/" element={<SearchParams />} />
-          </Routes>
-        </div>
+        <AdoptedPetContext.Provider value={adoptedPetHook}>
+          <div>
+            <header>
+              <Link to="/">Adopt Me!</Link>
+            </header>
+            <Routes>
+              <Route path="/details/:id" element={<Details />} />
+              <Route path="/" element={<SearchParams />} />
+            </Routes>
+          </div>
+        </AdoptedPetContext.Provider>
       </QueryClientProvider>
     </BrowserRouter>
   );
